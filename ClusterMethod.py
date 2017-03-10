48857342146eb772ac9e8e538744b7c42fef77e8
wang stephen <551619855@qq.com>
2017-03-10 21:45:35 +0800

    ...DataGenerator ok.

--- ClusterMethod.py
+++ ClusterMethod.py
@@ -6,6 +6,8 @@ import numpy as np
 import scipy as sp
 
 import matplotlib.pyplot as plt
+from matplotlib import cm
+
 import DataGenerator
 
 
@@ -17,6 +19,8 @@ def KMeans(Data,k=5):
     :return:
     '''
 
+    return np.zeros(Data.shape[0])
+
 def ISODATA(Data,k=5):
     '''
 
@@ -24,7 +28,7 @@ def ISODATA(Data,k=5):
     :param k:
     :return:
     '''
-    
+
 
 
 
@@ -33,3 +37,38 @@ if __name__ == '__main__':
     dg = DataGenerator.DataGenerator()
 
     test_data ,test_label = dg.HandleData(2000)
+    # plot resource data
+    test_fig = plt.figure()
+    ax = test_fig.add_subplot(131)
+    ax.grid(True)
+
+    for i in range(int(max(test_label)-min(test_label))+1):
+        index_list = list()
+        for k in range(test_label.shape[0]):
+            if test_label[k]==i:
+                index_list.append(k)
+        print(len(index_list))
+        ax.plot(test_data[index_list,0],test_data[index_list,1],'*',color=cm.jet(i*70+10))
+
+    # Kmeans Result
+    ax1=test_fig.add_subplot(132)
+    ax1.grid(True)
+
+    kmeans_lable = KMeans(test_data,5)
+
+
+    for i in range(int(max(kmeans_lable)-min(kmeans_lable))+1):
+        index_list = list()
+        for k in range(kmeans_lable.shape[0]):
+            if kmeans_lable[k] == i:
+                index_list.append(k)
+
+        ax1.plot(test_data[index_list,0],test_data[index_list,1],'+',color=cm.jet(i*70+10))
+
+
+
+
+
+
+
+    plt.show()
--- DataGenerator.py
+++ DataGenerator.py
@@ -19,7 +19,7 @@ class DataGenerator:
         # self.k=3
         # self.sigma=1.0
         # self.D=2
-        print("initial ok")
+        # print("initial ok")
 
     def NormalData(self,Data_number=100,k=3,offset=1.0,sigma=1.0,D=2):
         # Find centre points
@@ -39,18 +39,19 @@ class DataGenerator:
         centre_points[0,:]=[2.0,1.0]
         centre_points[1,:]=[1.0,2.0]
         centre_points[2,:]=[2.0,3.0]
-        centre_points[3,:]=[3.0,3.0]
+        centre_points[3,:]=[3.3,3.0]
         centre_points[4,:]=[4.0,2.0]
 
         step_length=int(numbers/centre_points.shape[0])
-        print("step lenght:",step_length)
+        # print("step lenght:",step_length)
 
 
 
         for i in range(centre_points.shape[0]):
-            print("i:",i)
+            # print("i:",i)
             DataSet[(step_length*i):(step_length*(i+1)),0]=np.random.normal(centre_points[i,0],sigma,DataSet[step_length*i:(step_length*(i+1)),0].shape)
             DataSet[(step_length*i):step_length*(i+1),1]=np.random.normal(centre_points[i,1],sigma,DataSet[step_length*i:(step_length*(i+1)),0].shape)
+            DataLabel[step_length*i:step_length*(i+1)]=i
 
         return DataSet,DataLabel
 
