9f5687f49f66ceb0651222fd42e1e7b5cfadf464
wang stephen <551619855@qq.com>
2017-03-10 20:27:33 +0800

    Initial commit

---
+++ DataGenerator.py
@@ -0,0 +1,37 @@
+# -*- coding:utf-8 -*-
+# carete by steve at  2017 / 03 / 10　20:05
+
+import numpy as np
+import scipy as sp
+
+import matplotlib.pyplot as plt
+
+class DataGenerator:
+    '''
+
+    '''
+    def __init__(self,k=3,sigma=1.0,D=2):
+        '''
+
+
+        '''
+
+        # self.k=3
+        # self.sigma=1.0
+        # self.D=2
+        print("initial ok")
+
+    def NormalData(self,Data_number=100,k=3,offset=1.0,sigma=1.0,D=2):
+        # Find centre points
+
+
+        # 
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    dg = DataGenerator()
